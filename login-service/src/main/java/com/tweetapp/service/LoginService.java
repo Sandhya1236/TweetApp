b415b58b4dfbee478807a4dc7a91a04ea792bbf2
Sandhya Adavikolanu <79351059+Sandhya1236@users.noreply.github.com>
2024-04-24 13:38:49 -0700

    Add files via upload

---
+++ login-service/src/main/java/com/tweetapp/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.tweetapp.service;
+
+import com.tweetapp.model.UserEntity;
+import com.tweetapp.request.ForgetPassword;
+import com.tweetapp.request.UserLogin;
+
+public interface LoginService {
+    public String registerUser(UserEntity userEntity);
+
+    public String login(UserLogin userLogin);
+
+    public String forgetPassword(String loginId,ForgetPassword forgetPassword) throws Exception;
+}
