2945fc545d9a91402e5a3063c38572230956bba2
Sandhya Adavikolanu <79351059+Sandhya1236@users.noreply.github.com>
2024-04-24 13:39:41 -0700

    Add files via upload

---
+++ tweet-action-service/src/main/java/com/tweetapp/TweetappApplication.java
@@ -0,0 +1,14 @@
+package com.tweetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class TweetappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetappApplication.class, args);
+    }
+
+}
